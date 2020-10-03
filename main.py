01386d635a4c846dfd9a801509e4e3b7798b9306
Игорь Неклюдов <igor.nekludovv@gmail.com>
2020-10-03 23:22:00 +0300

    Added time changing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from lxml import html
 TOKEN = 'token'
 bot = telebot.TeleBot(TOKEN)
 USERS = {}
-time = '23:44'
+TIME = {}
 
 def get_currency():
 	response = requests.get('https://yandex.ru')
@@ -27,7 +27,7 @@ def get_weather(city):
 	degrees = response.text[58:75].replace(' ','').replace('\n', '') # то же самое но с температурой
 	return f'Погода в г. {city} - {state}, {degrees}'
 
-def inform(chat_id):
+def inform(chat_id, time):
 	weather = get_weather(USERS[chat_id])
 	currency = get_currency()
 	if time >= '4:00' and time <= "12:00":
@@ -45,9 +45,9 @@ def check_time(USERS):
 	while True:
 		hours = int(datetime.datetime.utcnow().strftime('%H')) + 3
 		now = datetime.datetime.utcnow().strftime(f'{hours}:%M')
-		if now == time:
-			for user in USERS:
-				inform(user)
+		for user in USERS:
+			if now == TIME[user]:
+				inform(user, TIME[user])
 		timelib.sleep(60)
 
 thread = Thread(target=check_time, args=([USERS])) # создаем поток
@@ -56,8 +56,9 @@ thread = Thread(target=check_time, args=([USERS])) # создаем поток
 def start(message):
 	if message.chat.id not in USERS: # если пользователь не активировал бота
 		USERS[message.chat.id] = 'Москва'
-		second_part = f'\nКаждый день в {time} я буду сообщать тебе о последних новостях, погоде на улице и другую полезную информацию'
-		bot.send_message(message.chat.id, ("Привет, {0.first_name}!" + second_part).format(message.from_user, bot.get_me()), parse_mode='html')
+		TIME[message.chat.id] = '6:30'
+		second_part = '\nКаждый день в ' + TIME[message.chat.id] + ' я буду сообщать тебе о последних новостях, погоде на улице и другую полезную информацию\n\nКоманды бота:\n/city {город} - изменить город\n/time {время формата 6:30} - изменить время'
+		bot.send_message(message.chat.id, ("Привет, {0.first_name}!").format(message.from_user, bot.get_me())+second_part, parse_mode='html')
 		print('Бот активирован пользователем {0.first_name}'.format(message.from_user, bot.get_me())) # хоть какое-то подобие логирования в консоль
 		if not thread.is_alive():
 			thread.start()
@@ -68,11 +69,23 @@ def start(message):
 def slashcity(message):
 	global USERS
 	if message.text == '/city':
-		bot.send_message(message.chat.id, f'Текущий город - {USERS[message.chat.id]}', parse_mode='html')
+		bot.send_message(message.chat.id, 'Текущий город - ' + USERS[message.chat.id] + '\nИзменить город: /city {город}', parse_mode='html')
 	else:
 		local_city = message.text.replace('/city', '').replace(' ', '')
 		USERS[message.chat.id] = local_city
 		bot.send_message(message.chat.id, f'Вы успешно изменили город на {local_city}', parse_mode='html')
+		print(('{0.first_name} изменил(a) город на ' + local_city).format(message.from_user, bot.get_me()))
+
+@bot.message_handler(commands=['time'])
+def slashtime(message):
+	global TIME
+	if message.text == '/time':
+		bot.send_message(message.chat.id, 'Время отправки сообщения: ' + TIME[message.chat.id] + '\nИзменить время: /time {время формата 6:30}', parse_mode='html')
+	else:
+		new_time = message.text.replace('/time', '').replace(' ', '')
+		TIME[message.chat.id] = new_time
+		bot.send_message(message.chat.id, f'Вы успешно изменили время на {new_time}', parse_mode='html')
+		print(('{0.first_name} изменил(a) время на ' + new_time).format(message.from_user, bot.get_me()))
 
 if __name__ == '__main__':
 	bot.polling(none_stop=True) 
