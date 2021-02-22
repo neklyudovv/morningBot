fcde929d225fbb4a36d56480c71afef7df7f8f65
Игорь Неклюдов <igor.nekludovv@gmail.com>
2021-02-22 10:31:25 +0300

    Added bitcoin currency. Fixed weather bug and more

--- main.py
+++ main.py
@@ -5,29 +5,43 @@ import requests
 from threading import Thread
 from lxml import html
 
-TOKEN = 'token'
+TOKEN = 'token' # токен Телеграм бота 
 bot = telebot.TeleBot(TOKEN)
 USERS = {}
 TIME = {}
 
+# Курс биткоина
+def get_bitcoin_price():
+	response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+	data = response.json()
+	return (data["bpi"]["USD"]["rate"]).replace(',', '')[0:5]
+
+# Курсы евро, доллара и их обьединение в одну строку
 def get_currency():
 	response = requests.get('https://yandex.ru')
 	body = html.fromstring(response.text)
 	dollar = body.xpath('//span[@class="inline-stocks__value_inner"]/text()')[0]
 	euro = body.xpath('//span[@class="inline-stocks__value_inner"]/text()')[1]
-	return f'💵 Курсы валют:\n\n 💰1 EUR = {euro} RUB\n 💰1 USD = {dollar} RUB.'
+	btc = get_bitcoin_price()
+	return f'💵 Курсы валют:\n\n 💰1 EUR = {euro} RUB\n 💰1 USD = {dollar} RUB\n 💰1 btc = {btc} USD.'
 
+# Получение данных о погоде
 def get_weather(city):
-	url = f'http://wttr.in/{city}?0T'
+	url = f'http://wttr.in/{city}?http://wttr.in/kazan?0&format=j1&lang=ru&m&M'
 	request_headers = {
-    	'Accept-Language' : 'ru',
-    	'm' : ''
+		'0' : '',
+		'forman' : 'j1',
+    	'lang' : 'ru',
+    	'm' : '',
+    	'M' : '',
 	}
 	response = requests.get(url, headers=request_headers)
-	state = response.text[40:50].replace(' ','').replace('\n', '') # достаем из строки погоду и убираем пробелы с ентерами
-	degrees = response.text[58:75].replace(' ','').replace('\n', '') # то же самое но с температурой
-	return f'🌤 Погода в г. {city} - 🌡 {state},  {degrees}'
+	result = ((response.json())['current_condition'])[0]
+	state = result['lang_ru'][0]['value']
+	degrees = result['temp_C']
+	return f'🌤 Погода в г. {city} - 🌡 {state},  {degrees} ℃'
 
+# Получение пяти первых статей с Яндекса
 def get_news():
 	response = requests.get('https://yandex.ru')
 	body = html.fromstring(response.text)
@@ -37,20 +51,24 @@ def get_news():
 		result += f'📌 <a href="{news[i].attrib["href"]}">{news[i].text_content()}</a> \n'
 	return f'📰 Пока ты спал:\n{result}'
 
+# Отправка уже готового сообщения
 def inform(chat_id, time):
 	weather = get_weather(USERS[chat_id])
 	currency = get_currency()
-	if time >= '4:00' and time <= "12:00":
+	news = get_news()
+	now = int(time[0:2].replace(':', ''))
+	if now >= 4 and now <= 12:
 		first_part = 'Доброе утро!'
-	elif time >= "12:00" and time <= "18:00":
+	elif now >= 12 and now <= 18:
 		first_part = 'Добрый день!'
-	elif time >= "18:00" and time <= "23:00":
+	elif now >= 18 and now <= 23:
 		first_part = 'Добрый вечер!'
 	else:
 		first_part = 'Доброй ночи!'
-	bot.send_message(chat_id, f"{first_part} Время - {time}.\n\n{weather}\n\n{currency}\n\n{get_news()}", parse_mode='html')
+	bot.send_message(chat_id, f"{first_part} Время - {time}.\n\n{weather}\n\n{currency}\n\n{news}", parse_mode='html')
 	print('Сообщение отправлено!')
 
+# Ну чтобы не пропустить оптравку нужно и время проверять
 def check_time(USERS):
 	while True:
 		hours = int(datetime.datetime.utcnow().strftime('%H')) + 3
@@ -58,14 +76,14 @@ def check_time(USERS):
 		if len(now) == 5:
 		    if int(now[0:2]) >= 24:
 			    now == str(int(now[0:2]) - 21) + now[2:5]
-			    print(now)
 		for user in USERS:
 			if now == TIME[user]:
-				inform(user, TIME[user])
+				inform(user,TIME[user])
 		timelib.sleep(60)
 
 thread = Thread(target=check_time, args=([USERS])) # создаем поток
 
+# Обработка запуска бота
 @bot.message_handler(commands=['start'])
 def start(message):
 	if message.chat.id not in USERS: # если пользователь не активировал бота
@@ -73,13 +91,13 @@ def start(message):
 		TIME[message.chat.id] = '6:30'
 		second_part = '\nКаждый день в ' + TIME[message.chat.id] + ' я буду сообщать тебе о последних новостях, погоде на улице и другую полезную информацию\n\nКоманды бота:\n/city {город} - изменить город\n/time {время формата 6:30} - изменить время'
 		bot.send_message(message.chat.id, ("Привет, {0.first_name}!").format(message.from_user, bot.get_me())+second_part, parse_mode='html')
-		print('Бот активирован пользователем {0.first_name}'.format(message.from_user, bot.get_me())) # хоть какое-то подобие логирования в консоль
-		print(message)
+		print('Бот активирован пользователем {0.first_name}'.format(message.from_user, bot.get_me()))
 		if not thread.is_alive():
 			thread.start()
 	else:
 		bot.send_message(message.chat.id, (("Да активировал ты уже его, успокойся").format(message.from_user, bot.get_me())))
 
+# Обработка изменения города 
 @bot.message_handler(commands=['city'])
 def slashcity(message):
 	global USERS
@@ -98,6 +116,7 @@ def slashcity(message):
 		else:
 			bot.send_message(message.chat.id, f'Некорректный город', parse_mode='html')
 
+# Обработка изменения времени отправки сообщения
 @bot.message_handler(commands=['time'])
 def slashtime(message):
 	global TIME
@@ -108,9 +127,9 @@ def slashtime(message):
 		if len(new_time) == 5:
 			if new_time[2] == ':':
 				TIME[message.chat.id] = new_time
+				bot.send_message(message.chat.id, f'Вы успешно изменили время на {new_time}', parse_mode='html')
 			else:
 				bot.send_message(message.chat.id, f'Некорректное время', parse_mode='html')
-				bot.send_message(message.chat.id, f'Вы успешно изменили время на {new_time}', parse_mode='html')
 		else:
 			if new_time[1] == ':':
 				TIME[message.chat.id] = new_time
